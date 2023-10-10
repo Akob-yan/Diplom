71446f20eab6ddf80c9e1162eca79737656d1354
Akob <hakobhakobyan816@gmail.com>
2023-10-10 20:53:22 +0300

    update2

--- pages/menu.php
+++ pages/menu.php
@@ -1,3 +1,78 @@
+      <?php
+        if(isset($_SESSION['radmin'])){
+            if($page==6)
+                $c='active';
+                else $c='';
+                echo '<li class="'.$c.'"><a href="index.php?page=6">Private</a></li>';
+        }
+        ?>
+       
+<div class="menu">
+       
+        <li <?php echo ($page==4)? "class='active'":"" ?> class="answer">
+            <a href="index.php?page=4"> <p>To answer</p> </a>
+        </li>
+
+        <li <?php echo ($page==3)? "class='active'":"" ?> class="question">
+            <a href="index.php?page=3" > <p>Add a question</p> </a>
+        </li>
+
+        <?php
+        if(isset($_SESSION['radmin'])){
+            if($page==6)
+                $c='active';
+                else $c='';
+                echo '<li class="'.$c.'"><a href="index.php?page=6">Private</a></li>';
+        }
+        ?>
+</div>
+
+
+<style>
+    p{
+        font-size: 25px;
+        color: white;
+        padding: 6px;
+        
+    }
+    .menu{
+        background-color: black;
+        -webkit-border-radius: 30px;
+        -moz-border-radius: 30px;
+        border-radius: 30px;
+    }
+    .answer{
+        color: white;
+        float: left;
+        padding: 3px;
+        font-family: cursive;
+        
+    }
+    .question{
+        color: white;
+        float: right;
+        padding: 3px;
+        font-family: cursive;
+/*        font-family: Georgia, Verdana, "Trebuchet MS";*/
+    }
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!--
 <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
     <ul class="nav nav-tabs nav-justified" >
@@ -18,65 +93,6 @@
         <li <?php echo ($page==3)? "class='active'":"" ?> >
             <a href="index.php?page=3"> Registration </a>
         </li>
-
-        <?php
-        if(isset($_SESSION['radmin'])){
-            if($page==6)
-                $c='active';
-                else $c='';
-                echo '<li class="'.$c.'"><a href="index.php?page=6">Private</a></li>';
-        }
-        ?>
     </ul>
 </nav>
 -->
-
-
-       
-       
-       
-       
-       
-       
-<div class="menu">
-       
-        <li <?php echo ($page==4)? "class='active'":"" ?> class="adminforms">
-            <a href="index.php?page=4"> <p>Admin Forms</p> </a>
-        </li>
-
-        <li <?php echo ($page==3)? "class='active'":"" ?> class="registration">
-            <a href="index.php?page=3" > <p>Registration</p> </a>
-        </li>
-
-        <?php
-        if(isset($_SESSION['radmin'])){
-            if($page==6)
-                $c='active';
-                else $c='';
-                echo '<li class="'.$c.'"><a href="index.php?page=6">Private</a></li>';
-        }
-        ?>
-</div>
-
-
-<style>
-    p{
-        color: white;
-    }
-    .menu{
-        background-color: black;
-        -webkit-border-radius: 10px;
-        -moz-border-radius: 10px;
-        border-radius: 10px;
-    }
-    .adminforms{
-        color: white;
-        float: left;
-        
-    }
-    .registration{
-        color: white;
-        float: right;
-    }
-
-</style>
