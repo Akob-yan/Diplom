62876abc8d40522538947d72833f1967275490cb
Akob <hakobhakobyan816@gmail.com>
2023-10-30 15:36:45 +0300

    update

--- pages/admin.php
+++ pages/admin.php
@@ -1,5 +1,6 @@
-<div class="row">
-    <div class="col-sm-6 col-md-6 col-lg-6 left">
+<div class="">
+    <div>
+       <div class="diiv">
         <?php
         connect();
         $min = 0;
@@ -7,8 +8,8 @@
         $random = mt_rand($min,$max);
         $sel='select question from question where id='.$random;
         $res= mysql_query($sel);
-        echo '<form action="index.php?page=4" method="post" class="input-group" id="formquestion">';
-        echo '<table class="table table-striped">';
+        echo '<form action="index.php?page=4" method="post" class="" id="formquestion">';
+        echo '<table class="blok">';
         while($row=mysql_fetch_array($res, MYSQL_NUM)){
             echo '<tr>';
             echo '<td>'.$row[0].'</td>';
@@ -16,9 +17,10 @@
             echo '</tr>';
         }
         echo '</table>';
+        echo '</div>';
         mysql_free_result($res);
-        echo '<input type="text" name="question" placeholder="Answer">';
-        echo '<input type="submit" name="addquestion" value="Add" class="btn btn-sm btn-info">';
+        echo '<input type="text" name="question" placeholder="Ответ" class="answ">';
+        echo '<input type="submit" name="addquestion" value="Ответить" class="bttn">';
         echo '</form>';
         ?>
         <?php
@@ -27,12 +29,14 @@
             $ins='select answer from question where id='.$random;
             $answer = mysql_query($ins);
             $answer2=mysql_fetch_array($answer, MYSQL_NUM);
+            echo '<div class="pravN">';
             if($question == $answer2[0]){
                 echo "<p style='color:green'>Правильно</p>";
             }else{
                 echo "<p style='color:red'>НЕ правильно</p>";
             }
             echo '<input type="reset" value="Следующий вопрос" onclick="window.location.reload()" style="color:green">';
+            echo '</div>';
             
             
                 
@@ -42,6 +46,63 @@
 //            echo "</script>";
         }
         ?>
+        
+        <style>
+            .bttn{
+            margin-left: 550px;
+            margin-top: 200px;
+            margin: 0;
+            width: 120px;
+            height: 40px;
+            color: snow;
+            background: #6331eb;
+            position: absolute;
+            top: 40%;
+            left: 50%;
+            margin-right: -50%;
+            transform: translate(-50%, -50%) ;
+            border-radius: 10px;
+            -webkit-border-radius: 30px;
+            -moz-border-radius: 30px;
+            -khtml-border-radius: 30px;
+            border: 1px solid #000;
+            display: block;
+            }
+            .bttn:hover {
+                background-color: deepskyblue; /* Цвет фона при наведении */
+           }
+            .blok{
+                font-size: 2em;
+                margin-top: 50px;
+                margin-left: 265px;
+                background-color: gainsboro;
+            }
+            .diiv{
+                margin-left: 310px;
+                margin-right: 270px;
+                background-color: gainsboro;
+                -webkit-border-radius: 30px;
+                -moz-border-radius: 30px;
+                border-radius: 30px;
+                
+            }
+            .answ{
+            margin-left: 325px;
+            margin-top: 50px;
+            height: 50px;
+            width: 700px;
+            border-radius: 10px;
+            -webkit-border-radius: 10px;
+            -moz-border-radius: 10px;
+            -khtml-border-radius: 10px;
+            border: 1px solid #000;
+            display: block;
+            }
+            .pravN{
+                margin-left: 575px;
+                margin-top: 150px;
+            }
+        </style>
     </div>
     
 
